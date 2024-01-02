f2dc6ab984c896613982add63323c6f7e38b3da6
riyas1208 <riyasmu08@gmail.com>
2024-01-02 16:10:16 +0530

    commit

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -8,6 +8,6 @@ class AppColors {
   static const Color hintTextColor = Color(0xFFA0A0A0);
   static const Color errorColor = Colors.red;
   static const Color cardBackgroundColor = Color(0xF3F4F6FF);
-  static const Color favoriteIconColor = Color(0x624D8DFF);
-  static const Color bottomnavColor= Color(0xFFEDEDEF);
+  static const Color favoriteIconColor = Color(0xFF624D8D);
+  static const Color bottomnavColor = Color(0xFFEDEDEF);
 }
