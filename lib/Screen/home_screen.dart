84395f43d909c52985cddb287704c94059e8f458
riyas1208 <riyasmu08@gmail.com>
2024-01-03 12:37:50 +0530

    commit

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:project/data/data.dart';
 import 'package:project/util/colors.dart';
 import 'package:project/widget/product.dart';
+
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
 
@@ -10,49 +12,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
-
-  List<Product> products = [
-    Product(
-        name: 'Libas',
-        imageUrl: 'assets/a4.png',
-        price: 19.99,
-        description: "Ethnic Print Kurta"),
-    Product(
-        name: 'Soch',
-        imageUrl: 'assets/a5.png',
-        price: 29.39,
-        description: "Yellow & Pink Printed"),
-    Product(
-        name: 'Antheaa',
-        imageUrl: 'assets/a6.png',
-        price: 20.00,
-        description: "Green & Brown Floral"),
-    Product(
-        name: 'Janasya',
-        imageUrl: 'assets/a7.png',
-        price: 15.69,
-        description: "Pink Midi Dress"),
-    Product(
-        name: 'plusS',
-        imageUrl: 'assets/a8.png',
-        price: 17.59,
-        description: "Blue Floral Dress"),
-    Product(
-        name: 'Athena',
-        imageUrl: 'assets/a9.png',
-        price: 15.99,
-        description: "Pink Floral Dress"),
-    Product(
-        name: 'U&F',
-        imageUrl: 'assets/a0.png',
-        price: 19.99,
-        description: "Green Tiered Dress"),
-    Product(
-        name: 'plusS',
-        imageUrl: 'assets/a11.png',
-        price: 25.19,
-        description: "Polka-Dotted Dress"),
-  ];
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/data/data.dart
@@ -0,0 +1,44 @@
+import 'package:project/widget/product.dart';
+
+List<Product> products = [
+  Product(
+      name: 'Libas',
+      imageUrl: 'assets/a4.png',
+      price: 19.99,
+      description: "Ethnic Print Kurta"),
+  Product(
+      name: 'Soch',
+      imageUrl: 'assets/a5.png',
+      price: 29.39,
+      description: "Yellow & Pink Printed"),
+  Product(
+      name: 'Antheaa',
+      imageUrl: 'assets/a6.png',
+      price: 20.00,
+      description: "Green & Brown Floral"),
+  Product(
+      name: 'Janasya',
+      imageUrl: 'assets/a7.png',
+      price: 15.69,
+      description: "Pink Midi Dress"),
+  Product(
+      name: 'plusS',
+      imageUrl: 'assets/a8.png',
+      price: 17.59,
+      description: "Blue Floral Dress"),
+  Product(
+      name: 'Athena',
+      imageUrl: 'assets/a9.png',
+      price: 15.99,
+      description: "Pink Floral Dress"),
+  Product(
+      name: 'U&F',
+      imageUrl: 'assets/a0.png',
+      price: 19.99,
+      description: "Green Tiered Dress"),
+  Product(
+      name: 'plusS',
+      imageUrl: 'assets/a11.png',
+      price: 25.19,
+      description: "Polka-Dotted Dress"),
+];
